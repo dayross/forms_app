6dbea776d8d5c96d17b755f32466fc4132639f1f
Diana Zapata <dianarocioczapata@gmail.com>
2024-05-08 15:51:44 -0600

    form screen + added cubit for reg form

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import '../widgets/inputs/custom_text_form_field.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -15,29 +16,98 @@ class RegisterScreen extends StatelessWidget {
   }
 }
 
-class _RegisterView extends StatelessWidget {
+class _RegisterView extends StatefulWidget {
   const _RegisterView();
 
   @override
+  State<_RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<_RegisterView> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String user = '';
+  String email = '';
+  String passwd = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final colors = Theme.of(context).colorScheme;
+
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 30,),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20,),
+            
+                const FlutterLogo(size: 70,),
+                
+                const SizedBox(height: 30,),
+                
+                const Text('Registrate. Es gratis.',
+                  style: TextStyle(fontSize: 28),
+                ),
+                
+                const SizedBox(height: 30,),
+                
+                CustomTextFormField(
+                  hint: 'Nombre',
+                  // errorMessage: 'Ingrese un nombre valido.',
+                  onChanged:(value) => user = value,
+                  validator: (value) {
+                    if(value == null || value.isEmpty || value.trim().isEmpty) return 'Campo requerido'; 
+                    return null;
+                  },
+                ),
+                
+                const SizedBox(height: 20,),
+            
+                CustomTextFormField(
+                  hint: 'Correo',
+                  // errorMessage: 'Ingrese un correo valido.',
+                  onChanged:(value) => email = value,
+                  validator: (value) {
+                    if(value == null || value.isEmpty || value.trim().isEmpty) return 'Campo requerido'; 
+                    final emailRegExp = RegExp(
+                      r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                      );
 
-              FlutterLogo(size: 70,),
-
-              SizedBox(height: 30,),
-
-              _RegisterForm(),
-
-              SizedBox(height: 20,),
-              Text('Hola')
-            ],
+                    if(!emailRegExp.hasMatch(value)) return 'Ingrese un correo valido.';
+                  },
+                ),
+            
+                const SizedBox(height: 20,),
+            
+                CustomTextFormField(
+                  hint: 'Contraseña',
+                  // errorMessage: 'La contraseña debe de contener 6 caracteres como minimo.',
+                  obscure: true,
+                  onChanged:(value) => passwd = value,
+                  validator: (value) {
+                    if(value == null || value.isEmpty || value.trim().isEmpty) return 'Campo requerido'; 
+                    if(value.trim().length < 6 ) return 'Su contraseña debe contener 6 caracteres como minimo'; 
+                  },
+                ),
+                const SizedBox(height: 20,),
+            
+                FilledButton.icon(
+                  onPressed: () {
+                    final isValid = _formKey.currentState!.validate();
+                    if(!isValid) return;
+                    print('$user, $email, $passwd' );
+                  },
+                  icon: const Icon(Icons.done),
+                  label: const Text('Guardar'),
+            
+                  
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -53,13 +123,12 @@ class _RegisterForm extends StatelessWidget {
     return Form(
       child: Column(
         children: [
-          TextFormField(decoration: 
-                InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                    hintText: "Ingresa tu usuario"
-                ),
-              ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: "Ingresa tu usuario"),
+          ),
         ],
       ),
     );
