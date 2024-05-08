fbd8aa29d6adabb001f734872e7438d1f7b418ce
Diana Zapata <dianarocioczapata@gmail.com>
2024-05-08 10:13:55 -0600

    botones bloc

--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
   static const String name = 'cubits';
@@ -6,40 +8,73 @@ class BlocCounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: const BlocCounterView(),
+    );
+  }
+}
+
+class BlocCounterView extends StatelessWidget {
+  const BlocCounterView({
+    super.key,
+  });
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterBloc>().add(CounterIncreased(value));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: const Text('Bloc Counter'),
         actions: [
-          IconButton(onPressed: () => {}, icon: const Icon(Icons.refresh),)
-          ],),
-        body: const Center(
-          child: Text('Counter value: X'),),
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-              onPressed: (){},
-              heroTag: '1',
-              child: const Text('+1'),),
-
-              const SizedBox(height: 15,),
-
-              FloatingActionButton(
-              onPressed: (){},
-              heroTag: '2',
-              child: const Text('+2'),),
-            
-              const SizedBox(height: 15,),
-
-
-              FloatingActionButton(
-              onPressed: (){},
-              heroTag: '3',
-              child: const Text('+3'),),
-            
-            ],
+          IconButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+      body: Center(
+        child: context.select(
+          (CounterBloc counterBloc) =>
+              Text('Counter value: ${counterBloc.state.counter}'),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              increaseCounterBy(context);
+            },
+            heroTag: '1',
+            child: const Text('+1'),
           ),
-          // floatingActionButton: ,
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              increaseCounterBy(context, 2);
+            },
+            heroTag: '2',
+            child: const Text('+2'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              increaseCounterBy(context, 3);
+            },
+            heroTag: '3',
+            child: const Text('+3'),
+          ),
+        ],
+      ),
+      // floatingActionButton: ,
     );
   }
 }
