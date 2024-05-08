15e02617613bb7974e48ef24136648ade5f92dbb
Diana Zapata <dianarocioczapata@gmail.com>
2024-05-08 16:21:32 -0600

    cubit config for register form

--- lib/presentation/blocs/register/register_cubit.dart
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -3,6 +3,26 @@ import 'package:equatable/equatable.dart';
 
 part 'register_state.dart';
 
-class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() : super(RegisterInitial());
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(RegisterFormState());
+
+  void onSubmit(){
+    print('Submit: $state');
+  }
+
+  void userChanged(String value){
+    emit(state.copyWith(
+      user : value
+    ));
+  }
+  void emailChanged(String value){
+    emit(state.copyWith(
+      email : value
+    ));
+  }
+  void passwdChanged(String value){
+    emit(state.copyWith(
+      passwd : value
+    ));
+  }
 }
--- lib/presentation/blocs/register/register_state.dart
+++ lib/presentation/blocs/register/register_state.dart
@@ -1,10 +1,34 @@
 part of 'register_cubit.dart';
 
-sealed class RegisterState extends Equatable {
-  const RegisterState();
+// enumeracion de los estados que puede tener el formulario
+enum FormStatus {invalid, valid, validating, posting}
+class RegisterFormState extends Equatable {
+  final FormStatus formStatus;
+  final String user;
+  final String email;
+  final String passwd;
+
+  const RegisterFormState({
+    this.formStatus = FormStatus.invalid, 
+    this.user = '', 
+    this.email = '',
+    this.passwd = ''});
+
+  RegisterFormState copyWith({
+  FormStatus? formStatus,
+  String? user,
+  String? email,
+  String? passwd,
+  }) => RegisterFormState(
+  formStatus: formStatus ?? this.formStatus,
+  user: user ?? this.user,
+  email: email ?? this.email,
+  passwd: passwd ?? this.passwd
+
+  );
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [formStatus, user, email, passwd];
 }
 
-final class RegisterInitial extends RegisterState {}
+
